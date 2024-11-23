b9a3e757c4ad1c964b2959e4cf80f117ea68f81a
levlukas <256660@vutbr.cz>
2024-11-23 11:43:37 +0100

    bruteforce func added

--- bruteforce.c
+++ bruteforce.c
@@ -20,3 +20,17 @@ int is_clique(graph* g, int subset) {
     }
     return 1;  // great success
 }
+
+// find the greatest clique in the graph
+void bruteforce(graph* g) {
+    int max_clique = 0;  // size of the largest clique found
+    int max_subset = 0;  // subset representing the largest clique
+
+    // iterate over all possible subsets of vertices
+    int max_subset_size = 1 << g->size;  // 2^size -> binary shift
+    for (int subset = 1; subset < max_subset_size; subset++) {  // iterate over all subset sizes
+        if (is_clique(g, subset)) {
+
+        }
+    }
+}
