0576aa7f3a6ac10d438bb7286f692ad7e809df8d
levlukas <256660@vutbr.cz>
2024-11-27 12:53:48 +0100

    pridano zadani

--- main.c
+++ main.c
@@ -1,3 +1,48 @@
+/*
+ * ============================================================
+ * 
+ * NÁHRADNÍ PROJEKT - 09. NEJVĚTŠÍ KLIKA V NEORIENTOVANÉM GRAFU
+ * 
+ * ============================================================
+ * 
+ * Popis:
+ * Náhradní projekt je určen pouze pro studenty, kteří v předmětu
+ * IFJ neřeší souběžný projekt (např. studenti FEKT nebo studenti
+ * opakující předmět). Tento projekt je týmový a řeší jej trojice
+ * nebo čtveřice studentů.
+ *
+ * Zadání varianty:
+ * Klika grafu je podgraf, který je úplným grafem (=kterýkoliv
+ * vrchol kliky je tedy spojen hranou se všemi ostatními vrcholy
+ * kliky).
+ * 
+ * Vytvořte program pro hledání největší kliky v neorientovaném
+ * grafu. Pokud existuje více řešení, nalezněte všechna. Výsledky
+ * prezentujte vhodným způsobem. Součástí projektu bude načítání
+ * grafů ze souboru a vhodné testovací grafy. V dokumentaci uveďte
+ * teoretickou složitost úlohy a porovnejte ji s experimentálními
+ * výsledky.
+ * 
+ * Všeobecné informace a pokyny k náhradním projektům:
+ * Řešení bude vypracováno v jazyce C a bude přeložitelné (pomocí
+ * příkazu make) na serveru eva.fit.vutbr.cz. Všechny zdrojové kódy,
+ * hlavičkové soubory, testovací data aj. budou logicky separovány
+ * a uloženy v příhodně pojmenovaných podadresářích. Použití
+ * nestandardních knihoven není dovoleno. Všechny části zadání
+ * varianty jsou nutnou součástí řešení.
+ * 
+ * Celkové hodnocení projektu sestává z následujících kategorií:
+ * - funkčnost implementace (až 6 bodů),
+ * - projektová dokumentace (až 4 body),
+ * - obhajoba (až 5 bodů).
+ * 
+ * Řešení zabalené v jediném ZIP archivu je odevzdáváno pouze vedoucím
+ * týmu prostřednictvím STUDISu. Závazné pokyny pro vypracování projektové
+ * dokumentace a doporučení pro závěrečné obhajoby naleznete v Moodlu
+ * v sekci Projekty.
+ * 
+ */
+
 #include "graph.h"
 #include "algorithms/bruteforce.h"
 #include "algorithms/backtracking.h"
